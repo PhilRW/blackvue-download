a6041e12bc83e95b85112d9cc4b8b46ae8f2e5f5
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2018-02-27 18:04:23 -0700

    Initial commit

---
+++ blackvue-download.py
@@ -0,0 +1,69 @@
+import argparse
+import shutil
+
+import os
+
+import pathlib
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Download files from BlackVue camera")
+    parser.add_argument("host", help="the IP/hostname of the camera")
+    parser.add_argument("destination", help="the download directory")
+
+    args = parser.parse_args()
+
+    base = "http://" + args.host
+    url = "{0}/blackvue_vod.cgi".format(base)
+
+    if not os.path.isdir(args.destination):
+        print("destination directory {0} does not exist".format(args.destination))
+        sys.exit(1)
+
+    response = os.system("ping -c 1 " + args.host)
+    if response != 0:
+        print("host {0} is down".format(args.host))
+        sys.exit(2)
+
+    try:
+        skipped = 0
+        downloaded = 0
+
+        result = requests.get(url)
+
+        content = result.content.splitlines()
+        cam_files = []
+        for f in content:
+            if "Record" in f.decode():
+                video = f.decode().split(",")[0].split(":")[1]
+                cam_files.append(video)
+                if video.endswith("_NF.mp4"):
+                    basename = video[:-5]
+                    cam_files.append(basename + ".3gf")
+                    cam_files.append(basename + ".gps")
+
+        for f in cam_files:
+            fn = f.split("/")[-1]
+            y, m, d = fn[0:4], fn[4:6], fn[6:8]
+            dest_dir = os.path.join(args.destination, y, m, d)
+            if not os.path.isfile(os.path.join(dest_dir, fn)):
+                dest = os.path.join(args.destination, fn)
+                print("downloading {0} to {1} ...".format(f, dest))
+                r = requests.get(base + f, stream=True)
+                with open(dest + ".tmp", 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+
+                pathlib.Path(dest_dir).mkdir(parents=True, exist_ok=True)
+                os.rename(dest + ".tmp", os.path.join(dest_dir, fn))
+
+                downloaded += 1
+            else:
+                print("file {0} already downloaded, skipping".format(fn))
+                skipped += 1
+
+        print("{0} total, {1} skipped, {2} downloaded".format(len(cam_files), skipped, downloaded))
+
+    except requests.exceptions.ConnectionError as e:
+        print("Problem connecting to host {0}: {1}".format(args.host, e))
